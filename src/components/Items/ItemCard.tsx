c72e7f974731ddc47afbd772c82cc06d38e9b480
austin-baird_swi <austin.baird@solarwinds.com>
2026-05-20 14:28:08 -0400

    fix item card

--- src/components/Items/ItemCard.tsx
+++ src/components/Items/ItemCard.tsx
@@ -1,6 +1,12 @@
 import { useState, memo, JSX } from 'react';
 import { Item } from '@/types/database';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
 import {
@@ -55,13 +61,14 @@ export const ItemCard = memo(
       >
         <Card
           onDoubleClick={() => onEdit(item)}
-          className='overflow-hidden border-zinc-200 dark:border-zinc-700 transition-all hover:shadow-md h-full flex flex-col gap-0 py-1'
+          className='hover:cursor-pointer overflow-hidden border-zinc-200 dark:border-zinc-700 transition-all hover:shadow-md h-full flex flex-col gap-0 py-4 px-4'
         >
           <CardHeader className='pb-2 space-y-1 relative'>
-            <div className='flex justify-between items-start'>
+            <div className='flex justify-between items-center'>
               <CardTitle className='text-lg font-semibold text-zinc-900 dark:text-zinc-50 group-hover:text-green-600 transition-colors truncate'>
                 {item.name}
               </CardTitle>
+
               <span>
                 <Button
                   aria-label='Edit Button'
@@ -85,13 +92,26 @@ export const ItemCard = memo(
             </div>
             {status && (
               <Badge
+                style={{
+                  top: '-12px',
+                  left: '-12px',
+                  rotate: '-8deg',
+                }}
                 variant='secondary'
-                className={`${status.color} absolute top-7 left-4 text-white border-none py-0 px-2 h-5 text-[10px] uppercase font-bold`}
+                className={`${status.color} absolute text-white border-none py-0 px-2 h-5 text-[10px] uppercase font-bold`}
               >
                 {status.label}
               </Badge>
             )}
           </CardHeader>
+          <CardDescription className='text-right text-zinc-500 dark:text-zinc-400 px-4'>
+            {item.expiry_date && (
+              <div className='flex items-center text-xs text-zinc-500 dark:text-zinc-400'>
+                <Calendar className='h-3 w-3 mr-1' />
+                {format(new Date(item.expiry_date), 'MMM d')}
+              </div>
+            )}
+          </CardDescription>
           <div className='px-4 py-2 flex justify-center'>
             <div className='w-full h-64 rounded-xl overflow-hidden bg-zinc-100 dark:bg-zinc-800 flex items-center justify-center'>
               {!imgError ? (
@@ -111,15 +131,15 @@ export const ItemCard = memo(
             </div>
           </div>
           <CardContent className='mt-auto pt-2 pb-2'>
-            <div className='flex items-center justify-between'>
-              <div className='flex items-center space-x-2'>
+            <div className='flex items-center justify-center'>
+              <div className='flex w-full justify-between items-center space-x-3'>
                 <Button
                   variant='outline'
                   size='icon'
-                  className='h-7 w-7 rounded-full hover:cursor-pointer'
+                  className='h-10 w-16 hover:cursor-pointer'
                   onClick={() => onUpdateQuantity(item.id, item.quantity - 1)}
                 >
-                  <Minus />
+                  <Minus className='h-5 w-5' />
                 </Button>
                 <span className='text-sm font-medium w-12 text-center'>
                   {item.quantity}{' '}
@@ -131,18 +151,12 @@ export const ItemCard = memo(
                 <Button
                   variant='outline'
                   size='icon'
-                  className='h-7 w-7 rounded-full hover:cursor-pointer'
+                  className='h-10 w-16 hover:cursor-pointer'
                   onClick={() => onUpdateQuantity(item.id, item.quantity + 1)}
                 >
-                  <Plus />
+                  <Plus className='h-5 w-5' />
                 </Button>
               </div>
-              {item.expiry_date && (
-                <div className='flex items-center text-xs text-zinc-500 dark:text-zinc-400'>
-                  <Calendar className='h-3 w-3 mr-1' />
-                  {format(new Date(item.expiry_date), 'MMM d')}
-                </div>
-              )}
             </div>
           </CardContent>
         </Card>
